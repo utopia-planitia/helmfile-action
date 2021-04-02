63ed4bf40f382bd5f6e4a6d195ae589df6fb8b82
David Sauer <davedamoon@gmail.com>
2021-04-02 21:24:56 +0200

    skip not used arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
-helmfile --file="$1" --state-values-file="$2" template --args=--post-renderer="$3"
+helmfile=""
+values=""
+args=""
+
+if [ -z "$1" ]; then helmfile="--file='$1'"; fi
+if [ -z "$2" ]; then values="---state-values-file='$2'"; fi
+if [ -z "$3" ]; then args="--args=--post-renderer='$3'"; fi
+
+helmfile $helmfile $values template $args
