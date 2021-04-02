92e4e2304adb6b3e5a7eb62306517f355846f10c
David Sauer <davedamoon@gmail.com>
2021-04-02 21:41:14 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ values=""
 args=""
 
 if [ -n "$1" ]; then helmfile="--file=$1"; fi
-if [ -n "$2" ]; then values="---state-values-file=$2"; fi
+if [ -n "$2" ]; then values="--state-values-file=$2"; fi
 if [ -n "$3" ]; then args="--args=--post-renderer=$3"; fi
 
 echo helmfile $helmfile $values template $args
