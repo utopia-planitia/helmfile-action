f50f5fbb588df98e7dbf57ec7a0f694e33550a6b
David Sauer <davedamoon@gmail.com>
2021-04-02 21:32:10 +0200

    print command for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,6 @@ if [ -z "$1" ]; then helmfile="--file='$1'"; fi
 if [ -z "$2" ]; then values="---state-values-file='$2'"; fi
 if [ -z "$3" ]; then args="--args=--post-renderer='$3'"; fi
 
+echo helmfile $helmfile $values template $args
+
 helmfile $helmfile $values template $args
