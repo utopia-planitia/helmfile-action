16ff2d8d036f0187557ab77f4da57e097ecd8eeb
David Sauer <davedamoon@gmail.com>
2021-04-02 21:36:13 +0200

    fixed negation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ helmfile=""
 values=""
 args=""
 
-if [ -z "$1" ]; then helmfile="--file='$1'"; fi
-if [ -z "$2" ]; then values="---state-values-file='$2'"; fi
-if [ -z "$3" ]; then args="--args=--post-renderer='$3'"; fi
+if [ -n "$1" ]; then helmfile="--file='$1'"; fi
+if [ -n "$2" ]; then values="---state-values-file='$2'"; fi
+if [ -n "$3" ]; then args="--args=--post-renderer='$3'"; fi
 
 echo helmfile $helmfile $values template $args
 
